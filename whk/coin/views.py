2cc02aec969a70778bd7ced1fdd93979f0474540
KUNHEE SONG <skh4125@gmail.com>
2019-05-30 08:37:00 +0000

    190530 | api 가져오기

--- whk/coin/views.py
+++ whk/coin/views.py
@@ -3,50 +3,94 @@ import os
 import requests
 import configparser # 설정값 관리하기, 내장모듈
 from inspect import getcallargs
+import base64
+import hashlib
+import hmac
+import json
+import time
+import httplib2
 # Create your views here.
 
 access_token = os.getenv('access_token')
 secret_key = os.getenv('secret_key')
 
-url = "https://api.coinone.co.kr/v2/account/balance"
+ACCESS_TOKEN = str(access_token)
+SECRET_KEY = bytes(str(secret_key), 'utf-8')
 
-def set_token(self, grant_type="refresh_token"):
-    """
-    set token
-    grant_type: password, refresh_token
-    
-    saved self.access_token, self.refresh_token, self.token_type
-    """
-    token_api_path = "/oauth/refresh_token/"
-    
-    url_path = self.host + token_api_path
-    if grant_type == "refresh_token":
-        headers = {"content-type":"application/x-www-form-urlencoded"}
-        data = {"access_token":self.access_token}
-        config = configparser.ConfigParser()
-        config.read('conf/config.ini')
-        res = requests.post(url_path, headers=headers, data=data)
-        result = res.json()
-        self.access_token = result["accessToken"]
-        config["COINONE"]["access_token"] = self.access_token
-        with open('conf/config.ini', 'w') as configfile:
-            config.write(configfile)
-    else:
-        config = configparser.ConfigParser()
-        config.read('conf/config.ini')
-        self.access_token = config["COINONE"]["access_token"]
-        print("get config.ini")
-        print(self.access_token)
-    return self.access_token
-    
-def test_set_token(self):
-    print(inspect.stack()[0][3])
-    access_token = self.coinone_machine.set_token(grant_type="refresh_token")
-    assert access_token
+
+def get_encoded_payload(payload):
+    payload['nonce'] = int(time.time() * 1000)
+
+    dumped_json = json.dumps(payload)
+    encoded_json = base64.b64encode(bytes(dumped_json, 'utf-8'))
+    return encoded_json
+
+
+def get_signature(encoded_payload):
+    signature = hmac.new(SECRET_KEY, encoded_payload, hashlib.sha512)
+    return signature.hexdigest()
+
+
+def get_response(action, payload):
+    url = '{}{}'.format('https://api.coinone.co.kr/', action)
+
+    encoded_payload = get_encoded_payload(payload)
+
+    headers = {
+        'Content-type': 'application/json',
+        'X-COINONE-PAYLOAD': encoded_payload,
+        'X-COINONE-SIGNATURE': get_signature(encoded_payload),
+    }
+
+    http = httplib2.Http()
+    response, content = http.request(url, 'POST', body=encoded_payload, headers=headers)
+
+    return content
+
+
+# balance
+print(get_response(action='v2/account/balance', payload={
+    'access_token': ACCESS_TOKEN,
+}))
+
+# # limit buy
+# print(get_response(action='v2/order/limit_buy', payload={
+#     'access_token': ACCESS_TOKEN,
+#     'price': '4241000.0',
+#     'qty': '0.1',
+#     'currency': 'BTC',
+# }))
+
+# # limit sell
+# print(get_response(action='v2/order/limit_sell', payload={
+#     'access_token': ACCESS_TOKEN,
+#     'price': '4241000.0',
+#     'qty': '0.1',
+#     'currency': 'BTC',
+# }))
+
+# limit order
+print(get_response(action='v2/order/limit_orders', payload={
+    'access_token': ACCESS_TOKEN,
+    'currency': 'BTC',
+}))
+
+
+# cancel order
+# print(get_response(action='v2/order/cancel', payload={
+#     'access_token': ACCESS_TOKEN,
+#     'order_id': 'eeee8cab-a35a-43e5-8c47-d3c79c20d808',
+#     'price': '4241000.0',
+#     'qty': '0.1',
+#     'is_ask': '1',
+#     'currency': 'BTC',
+# }))
+
+
     
 # https://docs.python.org/3/library/inspect.html
 # https://wikidocs.net/12065
 
-# exec '$SHELL'
+# exec $SHELL
 # echo export 'token="<token>"' >> ~/.bashrc
 # c9 ~/.bashrc
